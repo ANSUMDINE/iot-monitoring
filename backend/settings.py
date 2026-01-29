54e6ca5e8e3d973ce4aa3008b2134d88fa98c3f9
ANSUMDINE <ansumdinesaidcombo@gmail.com>
2026-01-29 12:39:45 +0100

    Cors Headers

--- backend/settings.py
+++ backend/settings.py
@@ -62,8 +62,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
